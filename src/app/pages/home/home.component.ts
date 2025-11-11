479a61476ee5700deec16b5776670ba1af7a813f
Yanis Fischer <yfi126855@stud.gibb.ch>
2025-11-11 19:37:33 +0100

    responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,9 +13,9 @@ export class HomeComponent {
   news = [
     {
       id: 1,
-      title: 'Grümputurnier 21. März 2026',
+      title: 'Grümputurnier 25. April 2026',
       date: '21.03.2026',
-      summary: 'Am 21. März 2026 findet das erste Grümputurnier des UHC Gümmenen statt. Sei dabei!',
+      summary: 'Am 25. April 2026 findet das erste Grümputurnier des UHC Gümmenen statt. Sei dabei!',
       image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg?auto=compress&cs=tinysrgb&w=800',
       link: '/events'
     }
