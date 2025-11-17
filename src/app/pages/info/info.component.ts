5e0d1b01576d0d021185a998be2cf7697cc77e68
Yanis Fischer <yfi126855@stud.gibb.ch>
2025-11-17 15:20:08 +0100

    add info.component

---
+++ src/app/pages/info/info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-component',
+  imports: [],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css',
+})
+export class InfoComponent {
+
+}
